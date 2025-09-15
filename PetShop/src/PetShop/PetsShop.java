9005a75e8e19e0fcabae15cf8962dd0cbd7e01c6
lukasma02 <andersenlukas02@gmail.com>
2025-09-15 14:38:14 +0200

    moved

--- PetShop/src/PetShop/PetsShop.java
+++ PetShop/src/PetShop/PetsShop.java
@@ -3,9 +3,9 @@ package PetShop;
 public class PetsShop {
     public static void main(String[] args){
         Pets[] Pets = new Pets[3];
-        Pets[0] = new Pets("Dog", "Labrador", 3);
-        Pets[1] = new Pets("Cat", "Garfield", 14,);
-        Pets[2] = new Pets("Lizard", "Komodo Dragon", 67,);
+        Pets[0] = new Pets("Dog", "Labrador", 3,"Moved");
+        Pets[1] = new Pets("Cat", "Garfield", 14,"Moved");
+        Pets[2] = new Pets("Lizard", "Komodo Dragon", 67,"Moved");
 
         System.out.println("Pets available in the shop");
         for (Pets p : Pets){
